b75d90c5908b51b97c23dbbeec7b121520dbfa38
ma22-maker <67907256+ma22-maker@users.noreply.github.com>
2023-10-03 10:54:27 +0530

    DAY-2

---
+++ DAY-2 JS TRAINING/index.js
@@ -0,0 +1,99 @@
+
+
+// console.log(x);
+// //console.log(y);
+// var x=8;
+// let y=9;
+// const z=10;
+
+
+
+
+// sum(3,5);
+// multliply(5,2);
+// function  sum (a,b)
+// {
+//     console.log(a+b);
+// }
+
+
+
+// var multliply = function mul(a,b)
+// {
+//     console.log(a*b);
+// }
+
+
+
+
+// var animal="dog";
+// var sound = "Bow bow boww...."
+
+// function animals(a,b)
+// {
+//     console.log(`${a} barks ${b}`);
+// }
+// animals(animal,sound);
+
+
+
+
+
+// function a()
+// {  
+//     var a=5;
+//    // console.log(c); shows error 
+
+//     function b()
+//     {    a=2;
+//          var c=6;
+//         console.log(a);
+//         console.log(c);
+//     }
+//     b();
+//    console.log(a);
+   
+// }
+// a();
+
+
+// function x()
+// {
+//     var x=8;
+//     function y()
+//     {  
+//         console.log("inside y");
+//         console.log(x);
+//     }
+//     return y;
+// }
+// console.log(x());
+// var t=x();
+// t();
+
+var x=7;
+let q=25;
+const s=26;
+{
+    //console.log(q,s);
+   // s=19;
+    var d=2;
+    let b=4;
+    const c=12;
+    console.log(d);
+    console.log(b);
+    console.log(c);
+    let w=89;
+}
+console.log (b);
+console.log(c);
+
+// let a=9;
+// {
+//     let b=8;
+//     {
+//         let c=7;
+//         console.log(c,b,a);
+//     }
+// }
+
